a0e754e5200bee0b49641655cdfca29273a148f6
Samagra Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2026-03-30 21:07:49 +0530

    added premake file for the Beetle and the sandbox application

---
+++ premake5.lua
@@ -0,0 +1,111 @@
+workspace "Beetle"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Beetle"
+	location "Beetle"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"BT_PLATFORM_WINDOWS",
+			"BT_BUILD_DLL",
+
+
+		}
+
+		postbuildcommands
+		{
+			("{COPYFILE} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "BT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BT_Release"
+		symbols "On"
+
+	filter "configurations:DIST"
+		defines "BT_DIST"
+		symbols "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs
+	{
+		"Beetle/vendor/spdlog/include",
+		"Beetle/src"
+	}
+
+	links
+	{
+		"Beetle"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"BT_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "BT_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BT_Release"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "BT_DIST"
+		symbols "On"
