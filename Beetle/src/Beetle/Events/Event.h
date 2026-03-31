373d49a86084b1c7908d42dbf3099f2f3bfdc85a
Samagra Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2026-04-01 03:00:50 +0530

    completed the MouseEvent.h and KeyEvent.h files for their respective event handling and also created new file ApplicationEvent.h which helps in determining the window size, resize, close, open etc status also under the Event.h file added the dispatcher

--- Beetle/src/Beetle/Events/Event.h
+++ Beetle/src/Beetle/Events/Event.h
@@ -25,7 +25,7 @@ namespace Beetle {
 		EventCategoryMouseButton = BIT(4)
 	};
 
-#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() {return EventType::#type;}\
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() {return EventType::type;}\
 								virtual EventType GetEventType() const override {return GetStaticType();}\
 								virtual const char* GetName() const override {return #type;}
 
@@ -48,4 +48,29 @@ namespace Beetle {
 	protected:
 		bool m_Handled = false;
 	};
+
+	class EventDispatcher
+	{
+		template<typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event) : m_Event(event){}
+
+		template<typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 }
