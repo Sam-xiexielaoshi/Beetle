354694b7c2eeb679345f491c317ba8a51fe0722a
Samagra Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2026-04-21 16:24:36 +0530

    transform in a 3D rendering pipeline, usage: vertices define the shape of an object relative to itself, a transform (position, rotation, and scale) allows the engine to place multiple instances of the same model at different locations in the scene without needing unique vertex buffers for each one

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,5 +1,7 @@
 #include <Beetle.h>
 #include "imgui/imgui.h"
+#include <glm/gtc/matrix_transform.hpp>
+
 
 class ExampleLayer : public Beetle::Layer
 {
@@ -57,6 +59,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -65,7 +68,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -87,20 +90,22 @@ public:
 		m_Shader.reset(new Beetle::Shader(vertexSrc, fragmentSrc));
 
 		std::string blueShaderVertexSrc = R"(
-				#version 460 core
+			#version 460 core
+				
+			layout(location = 0) in vec3 a_Position;
 			
-				layout(location = 0) in vec3 a_Position;
+			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
-				uniform mat4 u_ViewProjection;
 
-				out vec3 v_Position;
+			out vec3 v_Position;
 			
-				void main()
-				{
-					v_Position = a_Position;
-					gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
-				}
-			)";
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+			}
+		)";
 
 		std::string blueShaderFragmentSrc = R"(
 				#version 460 core
@@ -132,12 +137,12 @@ public:
 		else if (Beetle::Input::IsKeyPressed(BT_KEY_DOWN))
 			m_CameraPosition.y -= m_CameraMoveSpeed * ts;
 
-		if (Beetle::Input::IsKeyPressed(BT_KEY_Q))
+		if (Beetle::Input::IsKeyPressed(BT_KEY_A))
 			m_CameraRotation += m_CameraRotationSpeed * ts;
 
-		else if (Beetle::Input::IsKeyPressed(BT_KEY_E))
+		else if (Beetle::Input::IsKeyPressed(BT_KEY_D))
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
-
+	 
 		Beetle::RendererCommand::SetClearColor({ 0.2f, 0.2f, 0.2f, 1.0f });
 		Beetle::RendererCommand::Clear();
 
@@ -145,9 +150,19 @@ public:
 		m_Camera.SetRotation(m_CameraRotation);
 
 		Beetle::Renderer::BeginScene(m_Camera);
-	
-		Beetle::Renderer::Submit(m_BlueShader, m_SquareVA);
-		Beetle::Renderer::Submit(m_Shader, m_VertexArray);
+
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+		
+		for (int i = 0; i < 5; i++) 
+		{
+			for (int j = 0; j < 20; j++)
+			{
+				glm::vec3 pos(i * 0.11f, j * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos)*scale;
+				Beetle::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		//Beetle::Renderer::Submit(m_Shader, m_VertexArray);
 		
 		Beetle::Renderer::EndScene();
 	}
@@ -175,6 +190,7 @@ private:
 
 	float m_CameraRotation = 0.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
 };
 
 class Sandbox : public Beetle::Application
