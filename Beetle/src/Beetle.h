711f4935c9006d1c367024ff9e7b4c6f144d3c5f
Samagra Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2026-04-21 13:08:27 +0530

    refactored most of the application.cpp and shifted it to sandbox.cpp successfully

--- Beetle/src/Beetle.h
+++ Beetle/src/Beetle.h
@@ -2,13 +2,26 @@
 //for us by beetle app
 
 #include "Beetle/Application.h"
-#include "Beetle/Input.h"
-#include "Beetle/KeyCodes.h"
 #include "Beetle/Layer.h"
 #include "Beetle/Log.h"
+
+#include "Beetle/Input.h"
+#include "Beetle/KeyCodes.h"
 #include "Beetle/MouseButtonCodes.h"
 
 #include "Beetle/ImGui/ImGuiLayer.h"
+
+//----------renderer Point---------------
+
+#include "Beetle/Renderer/Renderer.h"
+#include "Beetle/Renderer/RendererCommand.h"
+
+#include "Beetle/Renderer/Buffer.h"
+#include "Beetle/Renderer/Shader.h"
+#include "Beetle/Renderer/VertexArray.h"
+
+#include "Beetle/Renderer/OrthographicCamera.h"
+//----------End rednerer Point----------------
 
 //----------Entry Point----------------
 #include "Beetle/EntryPoint.h"
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -5,16 +5,132 @@ class ExampleLayer : public Beetle::Layer
 {
 public:
 	ExampleLayer()
-		: Layer("Example")
+		: Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f)
 	{
+		m_VertexArray.reset(Beetle::VertexArray::Create());
+
+		float vertices[3 * 7] = {
+			-0.5f , -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f
+		};
+
+		std::shared_ptr<Beetle::VertexBuffer> vertexBuffer;
+		vertexBuffer.reset(Beetle::VertexBuffer::Create(vertices, sizeof(vertices)));
+		Beetle::BufferLayout layout = {
+			{Beetle::ShaderDataType::Float3, "a_Position"},
+			{Beetle::ShaderDataType::Float4, "a_Color"}
+		};
+
+		vertexBuffer->SetLayout(layout);
+		m_VertexArray->AddVertexBuffer(vertexBuffer);
+
+		unsigned int indices[3] = { 0,1,2 };
+		std::shared_ptr<Beetle::IndexBuffer> indexBuffer;
+		indexBuffer.reset(Beetle::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
+		m_VertexArray->SetIndexBuffer(indexBuffer);
+
+		m_SquareVA.reset(Beetle::VertexArray::Create());
+		float squareVertices[3 * 4] = {
+			-0.5f , -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
+		};
+		std::shared_ptr<Beetle::VertexBuffer> squareVB;
+		squareVB.reset(Beetle::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
+
+		squareVB->SetLayout({
+			{Beetle::ShaderDataType::Float3, "a_Position"},
+			});
+		m_SquareVA->AddVertexBuffer(squareVB);
+
+		unsigned int squareIndices[6] = { 0,1,2,2,3,0 };
+		std::shared_ptr<Beetle::IndexBuffer> squareIB;
+		squareIB.reset(Beetle::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
+		m_SquareVA->SetIndexBuffer(squareIB);
+
+		std::string vertexSrc = R"(
+			#version 460 core
+			
+			layout(location = 0) in vec3 a_Position;
+			layout(location = 1) in vec4 a_Color;
+
+			uniform mat4 u_ViewProjection;
+
+			out vec3 v_Position;
+			out vec4 v_Color;
+			
+			void main()
+			{
+				v_Position = a_Position;
+				v_Color = a_Color;
+				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 460 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			in vec4 v_Color;
+			
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
+			}
+		)";
+
+		m_Shader.reset(new Beetle::Shader(vertexSrc, fragmentSrc));
+
+		std::string blueShaderVertexSrc = R"(
+				#version 460 core
+			
+				layout(location = 0) in vec3 a_Position;
+
+				uniform mat4 u_ViewProjection;
+
+				out vec3 v_Position;
+			
+				void main()
+				{
+					v_Position = a_Position;
+					gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				}
+			)";
+
+		std::string blueShaderFragmentSrc = R"(
+				#version 460 core
+			
+				layout(location = 0) out vec4 color;
+
+				in vec3 v_Position;
+			
+				void main()
+				{
+					color = vec4(0.2, 0.3, 0.8, 1.0);
+				}
+			)";
+		m_BlueShader.reset(new Beetle::Shader(blueShaderVertexSrc, blueShaderFragmentSrc));
 	}
 
 	void OnUpdate() override
 	{	
-		if (Beetle::Input::IsKeyPressed(BT_KEY_TAB))
-		{
-			BT_TRACE("Tab key is pressed (poll)!");
-		}
+		Beetle::RendererCommand::SetClearColor({ 0.2f, 0.2f, 0.2f, 1.0f });
+		Beetle::RendererCommand::Clear();
+
+		m_Camera.SetPosition({ 0.5f, 0.5f, 0.0f });
+		m_Camera.SetRotation(45.0f);
+
+		Beetle::Renderer::BeginScene(m_Camera);
+	
+		Beetle::Renderer::Submit(m_BlueShader, m_SquareVA);
+		Beetle::Renderer::Submit(m_Shader, m_VertexArray);
+		
+		Beetle::Renderer::EndScene();
 	}
 
 	void OnImGuiRender() override
@@ -34,6 +150,14 @@ public:
 			BT_TRACE("{0}", (char)e.GetKeyCode());
 		}
 	}
+private:
+	std::shared_ptr<Beetle::Shader> m_Shader;
+	std::shared_ptr<Beetle::VertexArray> m_VertexArray;
+
+	std::shared_ptr<Beetle::Shader> m_BlueShader;
+	std::shared_ptr<Beetle::VertexArray> m_SquareVA;
+
+	Beetle::OrthographicCamera m_Camera;
 };
 
 class Sandbox : public Beetle::Application
