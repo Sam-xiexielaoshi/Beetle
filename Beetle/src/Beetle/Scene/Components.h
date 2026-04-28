ffd6a5e2012b1ee3945ee5065dd700218f82e733
Samagra Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2026-04-29 01:41:13 +0530

    implement texture drag-and-drop functionality for 2D entities within the engine enabling users to load assets directly from the content browser into a sprite renderer component

--- Beetle/src/Beetle/Scene/Components.h
+++ Beetle/src/Beetle/Scene/Components.h
@@ -7,6 +7,7 @@
 
 #include "SceneCamera.h"
 #include "Beetle/Scene/ScriptableEntity.h"
+#include "Beetle/Renderer/Texture.h"
 
 
 namespace Beetle {
@@ -43,6 +44,8 @@ namespace Beetle {
 	struct SpriteRendererComponent
 	{
 		glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+		Ref<Texture2D> Texture;
+		float TilingFactor = 1.0f;
 
 		SpriteRendererComponent() = default;
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
