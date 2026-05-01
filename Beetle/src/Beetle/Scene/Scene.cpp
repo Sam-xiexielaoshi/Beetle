736e6f1915677754a7141dc4e13d818cbe33ed2c
Samagra Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2026-05-01 22:52:49 +0530

    testcode

--- Beetle/src/Beetle/Scene/Scene.cpp
+++ Beetle/src/Beetle/Scene/Scene.cpp
@@ -253,6 +253,7 @@ namespace Beetle {
 				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
 				Renderer2D::DrawSprite(transform.GetTransform(), sprite, (int)entity);
+				//Renderer2D::DrawRect(transform.GetTransform(), glm::vec4(1.0f), (int)entity);
 			}
 		}
 
@@ -262,7 +263,8 @@ namespace Beetle {
 				Renderer2D::DrawCircle(transform.GetTransform(), circle.Color, circle.Thickness, circle.Fade, (int)entity);
 				});
 		}
-		
+		/*Renderer2D::DrawLine(glm::vec3(2.0f), glm::vec3(5.0f), glm::vec4(1, 0, 1, 1));
+		Renderer2D::DrawRect(glm::vec3(0.0f), glm::vec2(1.0f), glm::vec4(1, 0, 1, 1));*/
 
 		Renderer2D::EndScene();
 	}
